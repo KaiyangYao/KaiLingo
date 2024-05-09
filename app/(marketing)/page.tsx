6e9c22d9c790a1246b4de021e5208cc10b2d0fe5
Kaiyang Yao <yaoky2008@gmail.com>
2024-05-09 02:53:06 -0400

    add logos on home page

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,3 +1,16 @@
+import Image from "next/image";
+
 export default function Home() {
-  return <div>This is a marketing page</div>;
+  return (
+    <div className="max-w-[998px] mx-auto flex-1 w-full flex flex-col lg:flex-row items-center justify-center p-4 gap-2">
+      <div className="relative w-[240px] h-[240px] lg:w-[424px] lg:h-[424px] mb-8 lg:mb-0">
+        <Image src="/next.svg" fill alt="Hero" />
+      </div>
+      <div className="flex flex-col items-center gap-y-8">
+        <h1 className="text-xl lg:text-3xl font-bold text-neutral-600 max-w-[480px] text-center">
+          Learn, practice, and master new languages with KaiLingo.
+        </h1>
+      </div>
+    </div>
+  );
 }
