6d87e47bedfb28301b8b17485a9e8f0bb42b1563
Kaiyang Yao <yaoky2008@gmail.com>
2024-05-08 22:20:07 -0400

    fix prettier issue

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,9 @@ export default function Home() {
   return (
     <div>
       <p className="text-green-500 font-bold">Hello Lingo!</p>
-      <Button size="lg" variant="default">Click me!</Button>
+      <Button size="lg" variant="default">
+        Click me!
+      </Button>
     </div>
-  )
+  );
 }
